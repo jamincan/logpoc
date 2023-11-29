5c38bff86c342bac3b4ba2bbc0cd67497b213ef1
Jeremy Haak <jamincan@gmail.com>
2023-11-28 21:02:47 -0500

    Fixed auth for Google

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,22 @@
 import prisma from '$lib/prisma';
+import { getSheetsClient } from '$lib/google';
+import { SPREADSHEET_ID } from '$env/static/private';
 
 export async function load() {
 	const logs = await prisma.log.findMany();
+    const sheets = getSheetsClient();
+    if (!sheets) return { logs};
+
+    try {
+		const response = await sheets.spreadsheets.values.get({
+			spreadsheetId: SPREADSHEET_ID,
+			range: 'A2:B'
+		});
+		const rows = (response.data.values ?? []) as [string, string][];
+		return { logs, rows };
+	} catch (e) {
+		console.log(e);
+        return { logs };
+	}
 	return { logs };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
 	export let data;
-	const { logs } = data;
+	const { logs, rows } = data;
 </script>
 
 <div>
-	<h1>Logs</h1>
+	<h1>Database Logs</h1>
 	<dl>
 		{#each logs as log (log.id)}
 			<dd>{log.date.toLocaleDateString()}</dd>
 			<dt>{log.note}</dt>
 		{/each}
 	</dl>
+    <h1>Sheet View</h1>
+    {#if rows}
+    <table>
+        {#each rows as row}
+            <tr><td>{row[0]}</td><td>{row[1]}</td></tr>
+        {/each}
+    </table>
+    {/if}
 </div>
