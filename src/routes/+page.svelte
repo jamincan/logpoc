e19a29597a667894ffacb88060265abd738dd8e9
Jeremy Haak <jeremy.haak@cronegeophysics.com>
2023-11-29 10:21:55 -0500

    Switch dd and dt for Database logs list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,16 +7,16 @@
 	<h1>Database Logs</h1>
 	<dl>
 		{#each logs as log (log.id)}
-			<dd>{log.date.toLocaleDateString()}</dd>
-			<dt>{log.note}</dt>
+			<dt>{log.date.toLocaleDateString()}</dt>
+			<dd>{log.note}</dd>
 		{/each}
 	</dl>
-    <h1>Sheet View</h1>
-    {#if rows}
-    <table>
-        {#each rows as row}
-            <tr><td>{row[0]}</td><td>{row[1]}</td></tr>
-        {/each}
-    </table>
-    {/if}
+	<h1>Sheet View</h1>
+	{#if rows}
+		<table>
+			{#each rows as row}
+				<tr><td>{row[0]}</td><td>{row[1]}</td></tr>
+			{/each}
+		</table>
+	{/if}
 </div>
